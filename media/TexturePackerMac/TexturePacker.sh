5f197d98013328cb665c63c897625d8c3725ce9c
manfeed <manfeed@users.noreply.github.com>
2022-01-28 17:43:28 +0100

    Changes for Frodo and some for Gotham
    Must be run in Gotham

--- media/TexturePackerMac/TexturePacker.sh
+++
@@ -1,2 +0,0 @@
-cd $(dirname $0)
-./TexturePacker -input ../Default -output ../Textures.xbt
--- media/TexturePackerWin/TexturePacker.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd %~dp0
-START /B /WAIT TexturePacker.exe -input ..\Default -output ..\Textures.xbt
-cd ../..
-
